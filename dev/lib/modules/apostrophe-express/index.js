387d9189e2c5eb9edba911bae369b5af843f8c71
Alexey <diver.stv@gmail.com>
2018-05-06 21:20:03 +0300

    Init commit

---
+++ dev/lib/modules/apostrophe-express/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  session: {
+    // If this still says `undefined`, set a real secret!
+    secret: '355197751c390474'
+  }
+};
---
+++ release/lib/modules/apostrophe-express/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  session: {
+    // If this still says `undefined`, set a real secret!
+    secret: '8d41dbfd26c35cbf'
+  }
+};
